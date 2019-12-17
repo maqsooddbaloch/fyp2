f0d35902ea2810bf8bae19c7bdea37a8e926d5e6
maqsoodbaloch1 <fa16bscs0012@maju.edu.pk>
2019-12-17 16:29:30 +0500

    db added

--- firebase/referaldata.js
+++ firebase/referaldata.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 async function sendreferal(){
     
     try{
@@ -79,8 +83,7 @@ try {
         adminid,
         venuename
     })
-    
-    location.assign('/planevepanel/argon-dashboard-master/examples/venues.html')
+    document.getElementById('input-space').value = ''
     
 } catch (error) {
     console.log(error.message)
@@ -89,19 +92,22 @@ try {
 }
 
 window.addEventListener('load',venuenames)
-async function venuenames(){
+function venuenames(){
 try {
     var table = document.getElementById('venuetable')
     var currentuser = localStorage.getItem('currentuser')
-    console.log(table)
-    var response = await firebase
-    .firestore()
-    .collection('venue-basic-info')
-    .where('adminid','==', currentuser)
-    .get()
-    response.forEach(element => {
 
-        table.innerHTML += `
+        firebase
+        .firestore()
+        .collection("venue-basic-info")
+        .where('adminid','==', currentuser)
+        .onSnapshot(snapshot => {
+          table.innerHTML = "";
+    
+         
+          snapshot.forEach(doc => {
+            const data = doc.data();
+                    table.innerHTML += `
         <tbody class="list" >
                             
         <tr>
@@ -111,7 +117,7 @@ try {
                       <img alt="Image placeholder" src="https://us.123rf.com/450wm/donets/donets1508/donets150800333/43440158-stock-vector-vector-user-icon-of-man-in-business-suit.jpg?ver=6">
                     </a>
                     <div class="media-body">
-                        <span class="mb-0 text-sm">${element.data().venuename}</span>
+                        <span class="mb-0 text-sm">${data.venuename}</span>
                     </div>
                 </div>
             </th>
@@ -129,14 +135,14 @@ try {
                     <div class="dropdown">
                         
                        
-                        <button  onclick = 'update("${element.id}")' class="btn btn-info"> update</button>
+                        <button  onclick = 'update("${doc.id}")' class="btn btn-info"> update</button>
                        
                     </div>
                 </td>
             <td class="text-right">
                 <div class="dropdown">
                     
-                    <button onclick = 'removevenue("${element.id}")' class="btn btn-danger"> Delete</button>
+                    <button onclick = 'removevenue("${doc.id}")' class="btn btn-danger"> Delete</button>
                 </div>
             </td>
             
@@ -147,10 +153,90 @@ try {
         
     </tbody>
 
-        
         `
+
+          });})
+
+
+
+
+//     table.innerHTML = '';
+//     console.log(table)
+//     firebase
+//     .firestore()
+//     .collection("venue-basic-info")
+//     .onSnapshot(snapshot => {
+     
+      
+//       snapshot.forEach(doc => {
+//         const data = doc.data();
        
-    });
+//         table.innerHTML += `
+//       <h1>${data.venuename}</h1>
+        
+//         `
+// console.log(data)
+//              });});
+
+      
+
+    // var response = await firebase
+    // .firestore()
+    // .collection('venue-basic-info')
+    // .where('adminid','==', currentuser)
+    // .get()
+    // response.forEach(element => {
+
+    //     table.innerHTML += `
+    //     <tbody class="list" >
+                            
+    //     <tr>
+    //         <th scope="row" class="name">
+    //             <div class="media align-items-center">
+    //                 <a href="#" class="avatar rounded-circle mr-3">
+    //                   <img alt="Image placeholder" src="https://us.123rf.com/450wm/donets/donets1508/donets150800333/43440158-stock-vector-vector-user-icon-of-man-in-business-suit.jpg?ver=6">
+    //                 </a>
+    //                 <div class="media-body">
+    //                     <span class="mb-0 text-sm">${element.data().venuename}</span>
+    //                 </div>
+    //             </div>
+    //         </th>
+           
+    //         <td class="status">
+    //             <br>
+    //             <label for="" class="" ><p>Unpublished</p></label>
+    //             <label class="custom-toggle" style="margin-bottom: -6px !important;">
+    //                 <input id="canbringfood" type="checkbox" >
+    //                 <span class="custom-toggle-slider rounded-circle"></span>
+    //               </label>
+    //         </td>
+   
+    //         <td class="text-right">
+    //                 <div class="dropdown">
+                        
+                       
+    //                     <button  onclick = 'update("${element.id}")' class="btn btn-info"> update</button>
+                       
+    //                 </div>
+    //             </td>
+    //         <td class="text-right">
+    //             <div class="dropdown">
+                    
+    //                 <button onclick = 'removevenue("${element.id}")' class="btn btn-danger"> Delete</button>
+    //             </div>
+    //         </td>
+            
+           
+    //     </tr>
+        
+      
+        
+    // </tbody>
+
+        
+    //     `
+       
+    // });
 } catch (error) {
     console.log(error.message)
 }
@@ -178,11 +264,17 @@ async function update(e){
 }
 
 
-async function updateitem(){
 
-var id = document.getElementById('ids')
-var crnt = localStorage.getItem('currentupdateitem')
-       var admid = localStorage.getItem('currentuser')
+
+
+function updateitem(){
+
+// var id = document.getElementById('ids')
+// // var img = localStorage.getItem('currentvenimage')
+ var img = document.getElementById('venue-image')
+
+ var crnt = localStorage.getItem('currentupdateitem')
+        var admid = localStorage.getItem('currentuser')
         var name = document.getElementById('venue-name').value;
         var vendescription = document.getElementById('venue-description').value;
         var address = document.getElementById('venue-address').value;
@@ -192,9 +284,22 @@ var crnt = localStorage.getItem('currentupdateitem')
         var city = document.getElementById('venue-city').value
         var postcode = document.getElementById('venue-postcode').value
         var nearest = document.getElementById('venue-nearest').value
-        
-        var respsonse = await firebase
-        .firestore()
+const imgname = Math.random().toString();
+const imageRef = firebase
+  .storage()
+  .ref()
+  .child(`images/${imgname}.png`);
+
+imageRef
+  .put(img.files[0])
+  .then(response => {
+    console.log("uploaded");
+    return imageRef.getDownloadURL();
+  })
+  .then(url => {
+   
+        firebase
+          .firestore()
         .collection('venue-basic-info')
         .doc(crnt)
         .set({
@@ -207,11 +312,17 @@ var crnt = localStorage.getItem('currentupdateitem')
             vencountry:country,
             vencity:city,
             venpostcode:postcode,
-            vennearest:nearest
+            vennearest:nearest,
+            profileimage:url
         })
 
+  
+  })
+  .catch(err => {
+    console.log("err =>", err);
+  });                      
+        
 }
-
 
 
 
@@ -224,10 +335,65 @@ try {
     .doc(e)
     .delete()
 
-    location.assign('/planevepanel/argon-dashboard-master/examples/venues.html')
-
+    
 } catch (error) {
     console.log(error.message)
 }
 
 }
+
+window.addEventListener('load',venueslist)
+async function venueslist(){
+try {
+    var element = document.getElementById('venuescards');
+
+
+
+ firebase
+    .firestore()
+    .collection("venue-basic-info")
+    .onSnapshot(snapshot => {
+      element.innerHTML = "";
+
+     
+      snapshot.forEach(doc => {
+        const data = doc.data();
+        console.log(data)
+                element.innerHTML += `
+                <div class="col-md-4">
+                <div class="card card-blog">
+                    <div class="card-image">
+                        <a href="./onevenue.html">
+                            <img class="img rounded" style="height: 200px;" src="${data.profileimage}">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h6 class="category text-primary">${data.venuename}</h6>
+
+                      
+                        <p class="card-description">
+                            As near as we can tell, this guy must have thought he was going over backwards and tapped the rear...
+                        </p>
+                        <div class="card-footer">
+                          
+                            <div class="stats stats-right">
+                                <i class="now-ui-icons tech_watch-time"></i> 5 min read
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      `
+      })
+    })
+
+} catch (error) {
+    console.log(error.message)
+}
+
+
+
+
+}
+
